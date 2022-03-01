52f1798ab8a6216c037e1cafca4678dfbb2becfb
Chris Kjolhede <ckjolhede@gmail.com>
2022-03-01 10:27:40 -0700

    test and feature: write #check_stock test and check_stock feature

---
+++ lib/vendor.rb
@@ -0,0 +1,15 @@
+require './lib/item'
+
+class Vendor
+  attr_reader :name, :inventory
+
+def initialize(name)
+  @name = name
+  @inventory = Hash.new(0)
+end
+
+def check_stock(item)
+  @inventory[:item]
+end
+
+end
--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -4,6 +4,8 @@ require "./lib/vendor"
 RSpec.describe Vendor do
   before(:each) do
     @vendor = Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
   end
 
 it "exists" do
@@ -18,5 +20,8 @@ it "has an empty hash created for inventory" do
   expect(@vendor.inventory).to eq({})
 end
 
+it "can count the number of items in inventory" do
+  expect(@vendor.check_stock(@item1)).to eq(0)
+end
 
 end
