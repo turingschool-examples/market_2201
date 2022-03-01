5289e6af45e3a5ca3dc3727e2894d259ad4dfccb
Andrew Speth <speth.andrew@yahoo.com>
2022-03-01 10:28:12 -0700

    feat: Vendor can check/add stock

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,8 +1,25 @@
 class Vendor
   attr_reader :name, :inventory
-  
+
   def initialize(name)
     @name = name
     @inventory = {}
+  end
+
+  def check_stock(item)
+    item = @inventory.find { |item| item }
+    if item.nil?
+      0
+    else
+      item[1]
+    end
+  end
+
+  def stock (item, count)
+    if !@inventory.key?(item)
+      @inventory[item] = count
+    else
+      @inventory[item] += count
+    end
   end
 end
