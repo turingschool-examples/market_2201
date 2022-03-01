af2c8de17558cd9b5d52cdd8699e937b4370520f
Johnny Bowman <johntbowman.production@gmail.com>
2022-03-01 10:21:56 -0700

    Feat: Vendor can check stock for specific item and add items to inventory

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -4,8 +4,16 @@ require './item'
 class Vendor
   attr_reader :name, :inventory
 
-  def initialize(name, inventory = {})
+  def initialize(name, inventory = Hash.new(0))
     @name = name
     @inventory = inventory
+  end
+
+  def stock(item, quantity)
+    @inventory[item] += quantity
+  end
+
+  def check_stock(item)
+    @inventory[item]
   end
 end
