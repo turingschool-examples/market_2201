38c8c492bc9ea7dd07087d23572badbe6020f316
Sherman Au <shermau13@gmail.com>
2022-03-01 10:11:34 -0700

    feat/refactor test:#exists

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,14 +1,14 @@
 require 'pry'
 require './lib/item'
 
-RSpec.describe  do
-  context "Iteration 1" do
+RSpec.describe do
+  context 'Iteration 1' do
     before(:each) do
-      @item = item = Item.new({name: 'Peach', price: "$0.75"})
+      @item = item = Item.new({ name: 'Peach', price: '$0.75' })
     end
 
     it 'exists' do
-      expect(@item)).to be_an_instance_of(Item)
+      expect(@item).to be_an_instance_of(Item)
     end
-
   end
+end
