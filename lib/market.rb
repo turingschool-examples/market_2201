d48382ecb8d487a9209481019ca8ff9c2c36ad00
Rin Fabian <pfabianschool@gmail.com>
2022-03-01 13:01:57 -0600

    time up

--- lib/market.rb
+++ lib/market.rb
@@ -22,4 +22,18 @@ class Market
   def vendors_that_sell(item)
     @vendors.select{|index| index.check_stock(item) != 0}
   end
+
+  def total_inventory
+    total_inventory = Hash.new {|key, value| key[value] = key.dup.clear}
+    @vendors.each do |index|
+      index.inventory.each_key do |key|
+        if total_inventory.include?(key)
+          total_inventory[key][:quantity] += index.inventory[key]
+        else
+          total_inventory[key][:quantity] = index.inventory[key]
+          total_inventory[key][:vendors].merge(index)
+        end
+      end
+    end
+  end
 end
--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -57,7 +57,7 @@ RSpec.describe Market do
   it 'can see potential_revenue' do
     expect(@vendor1.potential_revenue).to eq(29.75)
     expect(@vendor2.potential_revenue).to eq(345.00)
-    expect(@vendor3.potential_revenue).to eq(48.75)
+    expect(@vendor3.potential_revenue).to eq(101.75)
   end
 
   it 'can see total inventory of the market' do
