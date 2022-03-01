1e9108995b8ffe9731a75a542e2f5489d4cd5da8
Luke Pascale <lpluke5@gmail.com>
2022-03-01 10:18:35 -0700

    feat: Initialize Vendor class and write first test block

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Item do
   let(:item1) {Item.new({name: 'Peach', price: "$0.75"})}
   let(:item2) {Item.new({name: 'Tomato', price: '$0.50'})}
 
-  describe 'iteration 1' do
+  describe 'iteration 1 - item' do
 
     it 'exists' do
       expect(item1).to be_a(Item)
@@ -15,6 +15,6 @@ RSpec.describe Item do
       expect(item2.name).to eq('Tomato')
       expect(item2.price).to eq('$0.50')
     end
-    
+
   end
 end
---
+++ spec/vendor_spec.rb
@@ -0,0 +1,14 @@
+require './lib/item'
+require './lib/vendor'
+
+RSpec.describe Vendor do
+  let(:item1) {Item.new({name: 'Peach', price: "$0.75"})}
+  let(:item2) {Item.new({name: 'Tomato', price: '$0.50'})}
+  let(:vendor) {Vendor.new("Rocky Mountain Fresh")}
+
+  describe 'iteration 2  - Vendor' do
+    it 'exists and can read attributes' do
+      expect(vendor.name).to eq("Rocky Mountain Fresh")
+    end
+  end
+end
