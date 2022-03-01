9cab611be3a63b25c7395916df9095ec5d3a3355
Andrew Speth <speth.andrew@yahoo.com>
2022-03-01 11:39:46 -0700

    test: sorted item list

--- lib/market.rb
+++ lib/market.rb
@@ -19,4 +19,18 @@ class Market
   def vendors_that_sell(item)
     @vendors.find_all { |vendor| vendor.inventory.has_key?(item) }
   end
+
+  
+
+  # def total_inventory
+  #   total_inventory = {}
+  #   @vendors.each do |vendor|
+  #     vendor.inventory.each do |item|
+  #       if
+  #     require "pry"; binding.pry
+  #     end
+  #   end
+  #   total_inventory
+  #   # require "pry"; binding.pry
+  # end
 end
