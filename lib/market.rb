396c997677649731aa3eb45ee7d8cc9f3d8a918b
Will Medders <wmedders@gmail.com>
2022-03-01 12:49:47 -0500

    Spec: it exists

---
+++ lib/market.rb
@@ -0,0 +1,6 @@
+class Market
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+end
--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -1,12 +1,12 @@
-# require 'pry'
-# require 'rspec'
-# # require './lib/'
-#
-# RSpec.describe do
-#
-# it 'exists' do
-#   expect().to be_a()
-# end
-#
-#
-# end
+require 'pry'
+require 'rspec'
+require './lib/vendor.rb'
+require './lib/item.rb'
+require './lib/market.rb'
+
+RSpec.describe Market do
+  it 'exists' do
+    market = Market.new("South Pearl Street Farmers Market")
+    expect(market).to be_a(Market)
+  end
+end
