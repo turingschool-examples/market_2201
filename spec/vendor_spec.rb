621adfcb0a1bef601eee7b9636334dc94a221d07
Johnny Bowman <johntbowman.production@gmail.com>
2022-03-01 10:25:40 -0700

    Update: Vendor can check stock for specific item and add items to inventory

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -21,7 +21,17 @@ RSpec.describe Vendor do
     expect(@vendor.inventory).to eq({})
   end
 
-  it "can check inventory stock for specific item" do
+  it "can stock items and check inventory stock for specific item stock" do
     expect(@vendor.check_stock(@item1)).to eq(0)
+
+    @vendor.stock(@item1, 30)
+    expect(@vendor.check_stock(@item1)).to eq(30)
+    expect(@vendor.inventory).to eq({@item1 => 30})
+
+    @vendor.stock(@item1, 25)
+    expect(@vendor.check_stock(@item1)).to eq(55)
+
+    @vendor.stock(@item2, 12)
+    expect(@vendor.inventory).to eq({@item1 => 55, @item2 => 12})
   end
 end
