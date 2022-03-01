8cc864805e019f7719eeb5f8036dd2acc5959378
Sherman Au <shermau13@gmail.com>
2022-03-01 10:24:11 -0700

    #vendor class exists

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -7,11 +7,16 @@ RSpec.describe do
     before(:each) do
       @item1 = Item.new({ name: 'Peach', price: '$0.75' })
       @item2 = Item.new({ name: 'Tomato', price: '$0.50' })
+      @vendor = Vendor.new('Rocky Mountain Fresh')
     end
 
     it '#item attributes' do
       expect(@item2.name).to eq('Tomato')
       expect(@item2.price).to eq('$0.50')
+    end
+
+    it '#vendor exists' do
+      expect(@vendor).to be_a(Vendor)
     end
   end
 end
