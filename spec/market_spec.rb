61a229065962f37c640834f2ba87d8cdc5970319
Andrew Speth <speth.andrew@yahoo.com>
2022-03-01 11:46:17 -0700

    test: overstocked items

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -79,6 +79,28 @@ RSpec.describe Market do
     expect(market.sorted_item_list).to eq(["Banana Nice Cream", "Peach", "Peach-Raspberry Nice Cream", "Tomato"])
   end
 
+  it "can return overstocked items" do
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    vendor3.stock(item1, 65)
+
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+
+    expect(market.overstocked_items).to eq([item1])
+  end
+
 
   # it "can return total inventory" do
   #   market = Market.new("South Pearl Street Farmers Market")
