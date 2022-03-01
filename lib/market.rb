ae539c900ca2dfaab5acb8b027e209c80a162154
Kim G <xiaole.guo@yahoo.com>
2022-03-01 10:31:20 -0700

    Test: Market can add vendors

--- lib/market.rb
+++ lib/market.rb
@@ -1,10 +1,13 @@
 class Market
-attr_reader :name, :vendors
+    attr_reader :name, :vendors
 
-def initialize(name)
-  @name = name
-  @vendors = []
-end
+    def initialize(name)
+      @name = name
+      @vendors = []
+    end
 
+    def add_vendor(vendor)
+      @vendors << vendor
+    end
 
 end
