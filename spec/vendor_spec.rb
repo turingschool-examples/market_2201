9727a401a4d2702d20d21df0105bca726724321b
Susan Boyd <sueboyd922@gmail.com>
2022-03-01 12:44:43 -0500

    finish iteration 2

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -56,6 +56,8 @@ describe Vendor do
       @vendor3.stock(@item1, 65)
 
       expect(@vendor1.potential_revenue).to eq(29.75)
+      expect(@vendor2.potential_revenue).to eq(345.00)
+      expect(@vendor3.potential_revenue).to eq(48.75)
     end
   end
 
