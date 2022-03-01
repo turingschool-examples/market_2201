80d88e7198edadb55db15595a104eb5e6949ae2a
Andrew Speth <speth.andrew@yahoo.com>
2022-03-01 11:58:39 -0700

    feat: overstocked items

--- lib/market.rb
+++ lib/market.rb
@@ -32,15 +32,28 @@ class Market
     sorted_item_list.sort.uniq
   end
 
-  # def total_inventory
-  #   total_inventory = {}
-  #   @vendors.each do |vendor|
-  #     vendor.inventory.each do |item|
-  #       if
-  #     require "pry"; binding.pry
-  #     end
-  #   end
-  #   total_inventory
-  #   # require "pry"; binding.pry
-  # end
+  def overstocked_items
+    overstocked_items = []
+    items_over_100 = []
+    overstock_hash = {}
+    @vendors.each do |vendor|
+      vendor.inventory.each do |item, count|
+        overstock_hash[item] = count if !overstock_hash.has_key?(item)
+        overstock_hash[item] += count
+      end
+    end
+    items_over_100 << overstock_hash.find_all { |item, count| count > 100 }
+    overstocked_items << items_over_100[0].flatten[0]
+  end
+
+  def total_inventory
+    total_inventory = {}
+    @vendors.each do |vendor|
+      vendor.inventory.each do |item, count|
+        total_inventory[item] = count if !total_inventory.has_key?(item)
+        total_inventory[item] += count
+      end
+    end
+    total_inventory
+  end
 end
