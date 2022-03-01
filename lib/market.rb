63c3953342dc1656a88dc6116443d38c5be249e0
Phillip Kamps <phillip.kamps@gmail.com>
2022-03-01 11:15:33 -0700

    Test: Market #add_vendor + #vendor_name, Feat: Market #add_vendor + #vendor_name

--- lib/market.rb
+++ lib/market.rb
@@ -1,7 +1,19 @@
 class Market
-  attr_reader :name, :vendors
+  attr_accessor :name, :vendors
   def initialize(name)
     @name = name
     @vendors = []
+  end
+
+  def add_vendor(vendor)
+    vendors << vendor
+  end
+
+  def vendor_names
+    vendor_names = []
+    vendors.each do |vendor|
+      vendor_names << vendor.name
+    end
+    vendor_names
   end
 end
--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -18,4 +18,23 @@ RSpec.describe Market do
   it "reads market vendors" do
     expect(market.vendors).to eq([])
   end
+
+  it "adds vendors" do
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    vendor3.stock(item1, 65)
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+    expect(market.vendors).to eq([vendor1, vendor2, vendor3])
+    expect(market.vendor_names).to eq(["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+  end
+
+  it "reads vendor names" do
+    expect(market.vendor_names).to eq(["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+  end
 end
