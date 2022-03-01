f9f93e256a714a1675943235ac6b356714703dd4
Chris Romero <chrisleeromero1@gmail.com>
2022-03-01 12:00:56 -0700

    Test: add overstocked item test

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -105,9 +105,10 @@ RSpec.describe Market do
        expect(vendor3.potential_revenue).to eq(48.75)
     end
 
-    it "can display items sold" do
+    xit "can display items sold" do
       item1 = Item.new({name: "Peach", price: "$0.75"})
       item2 = Item.new({name: "Tomato", price: "$0.50"})
+      item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
       item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
       vendor1 = Vendor.new("Rocky Mountain Fresh")
       vendor1.stock(item1, 35)
@@ -118,10 +119,29 @@ RSpec.describe Market do
       vendor3 = Vendor.new("Palisade Peach Shack")
       vendor3.stock(item1, 65)
       vendor3.stock(item3, 10)
-      market.add_vendor(vendor1)
-      market.add_vendor(vendor2)
-      market.add_vendor(vendor3)
-      expect(@market.total_inventory).to eq()
+      @market.add_vendor(vendor1)
+      @market.add_vendor(vendor2)
+      @market.add_vendor(vendor3)
+      expect(@market.total_inventory).to eq(
+        {
+          item1 => {
+            quantity: 100,
+            vendors: [vendor1, vendor3]
+          },
+          item2 => {
+            quantity: 7,
+            vendors: [vendor1]
+          },
+          item3 => {
+             quantity: 50,
+             vendors: [vendor3]
+           },
+           item4 => {
+              quantity: 35,
+              vendors: [vendor2, vendor3]
+              },
+            }
+      expect(@market.overstocked_items).to eq(item2)
     end
 
   end
