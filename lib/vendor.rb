222ab7c62b43a17466275118c71edf90bbe8e91d
Chris Romero <chrisleeromero1@gmail.com>
2022-03-01 10:30:59 -0700

    Feat: add code for add items to inventory hash

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -3,11 +3,15 @@ class Vendor
   attr_reader :name, :inventory
   def initialize(name)
     @name = name
-    @inventory = {}
+    @inventory = Hash.new(0)
   end
 
   def check_stock(item)
     0
+  end
+
+  def stock(item, quantity)
+    @inventory[item] = quantity
   end
 
 end
