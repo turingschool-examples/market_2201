720614a52f72d268d18e84772f9ad770b0b1006d
Phillip Kamps <phillip.kamps@gmail.com>
2022-03-01 10:54:54 -0700

    Test: Market #vendors

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -5,8 +5,17 @@ require "pry"
 
 RSpec.describe Market do
   market = Market.new("South Pearl Street Farmers Market")
+  vendor1 = Vendor.new("Rocky Mountain Fresh")
+  item1 = Item.new({name: "Peach", price: "$0.75"})
+  item2 = Item.new({name: "Tomato", price: "$0.50"})
+  item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+  item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
 
   it "reads market name" do
     expect(market.name).to eq("South Pearl Street Farmers Market")
+  end
+
+  it "reads market vendors" do
+    expect(market.vendors).to eq([])
   end
 end
