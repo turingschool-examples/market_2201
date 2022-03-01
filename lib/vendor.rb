7e32671d7f70cb6ab60c3e12e6169b3a586a2074
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-01 10:46:26 -0700

    Feat: potential_revenue passing test

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -21,4 +21,14 @@ class Vendor
     @inventory[item] += amount
     @inventory[item]
   end
+
+  def potential_revenue
+    total_revenue = 0
+    @inventory.each do |item, amount|
+      price = item.price.delete("$")
+      price_float = price.to_f
+      total_revenue += (price_float) * amount
+    end
+    total_revenue.round(2)
+  end
 end
