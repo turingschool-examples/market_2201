842de9d360558722b8199995a75595b5d753392a
Will Medders <wmedders@gmail.com>
2022-03-01 12:10:16 -0500

    Item name and price

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,12 +1,20 @@
-# require 'pry'
-# require 'rspec'
-# # require './lib/'
-#
-# RSpec.describe do
-#
-# it 'exists' do
-#   expect().to be_a()
-# end
-#
-#
-# end
+require 'pry'
+require 'rspec'
+require './lib/item.rb'
+
+RSpec.describe do
+
+  it 'exists' do
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    expect(item1).to be_a(Item)
+  end
+
+  it 'has a name and price' do
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    expect(item1.name).to eq('Peach')
+    expect(item1.price).to eq("$0.75")
+  end  
+
+
+
+end
