b4c81579bd7bb14566a81a752133cb91b7a7d463
Adrian Campos <adriancampos2992@yahoo.com>
2022-03-01 09:25:12 -0800

    Test: pass vendor can ehck stock of items with item pass in to eq 0

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,10 +1,16 @@
 class Vendor
 
-  attr_reader :name, :inventory
+  attr_reader :name, :inventory, :check_stock
 
   def initialize(name)
     @name = name
     @inventory = Hash.new(0)
+    @check_stock = 0
+  end
+
+
+  def check_stock(item)
+    @check_stock
   end
 
 end
--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -11,6 +11,17 @@ RSpec.describe Vendor do
     expect(vendor.inventory).to eq({})
   end
 
+  it 'vendor can check_stock of items in inventory' do
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    expect(vendor.check_stock(item1)).to eq(0)
+  end
 
-
+  xit 'vendor can check_stock of items in inventory' do
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    vendor.check_stock(item1)
+    vendor.stock(item1, 30)
+    expect(vendor.inventory).to eq(item1, 30)
+  end
 end
