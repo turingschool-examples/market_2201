223b81cceb3db3b8f4933278cad4e6bb9003f354
Andrew Speth <speth.andrew@yahoo.com>
2022-03-01 10:22:00 -0700

    test: Vendor can check/add stock

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -8,4 +8,21 @@ RSpec.describe Vendor do
     expect(vendor.name).to eq("Rocky Mountain Fresh")
     expect(vendor.inventory).to eq({})
   end
+
+  it "can check/add stock" do
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
+    expect(vendor.check_stock(item1)).to eq(0)
+    vendor.stock(item1, 30)
+    expect(vendor.inventory).to eq({item1 => 30})
+    expect(vendor.check_stock(item1)).to eq(30)
+
+    vendor.stock(item1, 25)
+    expect(vendor.check_stock(item1)).to eq(55)
+
+    vendor.stock(item2, 12)
+    expect(vendor.inventory).to eq({item1 => 55, item2 => 12})
+  end
 end
