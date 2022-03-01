f5e9a3352f467c5a39cf03d37cc1b000e0799be7
Sherman Au <shermau13@gmail.com>
2022-03-01 10:05:43 -0700

    test:#item exists

---
+++ spec/item_spec.rb
@@ -0,0 +1,14 @@
+require 'pry'
+require './lib/item'
+
+RSpec.describe  do
+  context "Iteration 1" do
+    before(:each) do
+      @item = item = Item.new({name: 'Peach', price: "$0.75"})
+    end
+
+    it 'exists' do
+      expect(@item)).to be_an_instance_of(Item)
+    end
+
+  end
