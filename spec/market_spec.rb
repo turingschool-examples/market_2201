a0ed61c1e0d9396d03340d6daf9f7b3fe30e3dc8
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-01 10:59:16 -0700

    Test: write test for vendor_names

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -13,7 +13,7 @@ describe Market do
     end
   end
 
-  describe 'add_vendor' do
+  describe 'vendor management' do
     market = Market.new("South Pearl Street Farmers Market")
     vendor1 = Vendor.new("Rocky Mountain Fresh")
     vendor2 = Vendor.new("Ba-Nom-a-Nom")
@@ -27,11 +27,16 @@ describe Market do
     vendor2.stock(item4, 50)
     vendor2.stock(item3, 25)
     vendor3.stock(item1, 65)
-    it 'adds a given vendor to the vendors array' do
+
+    it 'can add a vendor with add_vendor' do
       market.add_vendor(vendor1)
       market.add_vendor(vendor2)
       market.add_vendor(vendor3)
       expect(market.vendors).to eq([vendor1, vendor2, vendor3])
+    end
+
+    it 'can return all vendor names' do
+      expect(market.vendor_names).to eq(["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
     end
   end
 end
