f2a47182f1f3c613278511413e2c1d2c6bbe8379
Chris Romero <chrisleeromero1@gmail.com>
2022-03-01 10:42:52 -0700

    Feat: add passing code to market class for initialize method

--- lib/market.rb
+++ lib/market.rb
@@ -1,23 +1,7 @@
-require './lib/vendor'
-require './lib/item'
-require './lib/market'
+class Market
 
-RSpec.describe Market do
-
-  before(:each) do
-    @market = Market.new("South Pearl Street Farmers Market")
+  def initialize(name)
+    # @name = name
   end
-
-  describe 'Iteration 2' do
-
-    it "exists" do
-      expect(@market).to be_a(Market)
-    end
-
-
-  end
-
-
-
 
 end
