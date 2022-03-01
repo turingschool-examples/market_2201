1271d4fc3c728b1fe6eec10f7ad945fb66c942f6
Chris Romero <chrisleeromero1@gmail.com>
2022-03-01 10:15:47 -0700

    Feat: add passing code for price method

--- lib/item.rb
+++ lib/item.rb
@@ -1,8 +1,9 @@
 class Item
-  attr_reader :name
+  attr_reader :name, :price
   def initialize(details)
     @details = details
     @name = details[:name]
+    @price = details[:price][1..-1].to_f
   end
 
 end
