1af75e668ab3ccb02f28c493510573c281bd92dd
Rin Fabian <pfabianschool@gmail.com>
2022-03-01 11:14:33 -0600

    feat; created an initialize method for the item class that sets each items name and price

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,6 +1,6 @@
 require './lib/item'
 
-RSpec.describ Item do
+RSpec.describe Item do
   before(:each) do
     @item1 = Item.new({name: 'Peach', price: "$0.75"})
     @item2 = Item.new({name: 'Tomato', price: '$0.50'})
