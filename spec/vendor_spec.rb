1b5b28665177cc8e6547f6eb65f6f1a844f4159a
Chris Kjolhede <ckjolhede@gmail.com>
2022-03-01 10:43:10 -0700

    test: write test to return inventory elements

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -35,6 +35,14 @@ RSpec.describe Vendor do
     expect(@vendor.check_stock(@item1)).to eq(55)
   end
 
+  it 'can return inventory elements' do
+    @vendor.stock(@item1, 25)
+    @vendor.stock(@item1, 30)
+    @vendor.stock(@item2, 12)
+    expect(@vendor.inventory).to eq({@item1 => 55, @item2 => 12})
+  end
+
+
 
 
 end
