50a88a2a8ab76ac000ff055f52f078e92b25fe13
Will Medders <wmedders@gmail.com>
2022-03-01 12:50:51 -0500

    Spec it has a name

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -9,4 +9,9 @@ RSpec.describe Market do
     market = Market.new("South Pearl Street Farmers Market")
     expect(market).to be_a(Market)
   end
+
+  it 'has a name' do
+    market = Market.new("South Pearl Street Farmers Market")
+    expect(market.name).to eq("South Pearl Street Farmers Market")
+  end
 end
