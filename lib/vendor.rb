a1b8fe30cb433ee33334ae72df25d5c0c9f928ee
alex randolph <alexgeorgerandolph@gmail.com>
2022-03-01 10:42:46 -0700

    Add: iteration 1

---
+++ lib/vendor.rb
@@ -0,0 +1,25 @@
+require './lib/item'
+
+class Vendor
+
+  attr_reader :name, :inventory
+
+  def initialize(name)
+    @name = name
+    @inventory = Hash.new(0)
+  end
+
+  def check_stock(item)
+    if @inventory.has_key?(item)
+      @inventory[item]
+    else
+      return 0
+    end
+  end
+
+  def stock(item, amount)
+    @inventory[item] += amount
+  end
+
+
+end
