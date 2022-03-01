6e9e9ff2011f4ee0c09567008d58adf292600fd0
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-01 11:13:50 -0700

    Test: write test for total_inventory

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -26,7 +26,6 @@ describe Market do
     vendor1.stock(item2, 7)
     vendor2.stock(item4, 50)
     vendor2.stock(item3, 25)
-    vendor3.stock(item1, 65)
 
     it 'can add a vendor with add_vendor' do
       market.add_vendor(vendor1)
@@ -42,6 +41,35 @@ describe Market do
     it 'vendors_that_sell(item) returns an array of vendors' do
       expect(market.vendors_that_sell(item1)).to eq([vendor1, vendor3])
       expect(market.vendors_that_sell(item4)).to eq([vendor2])
+    end
+  end
+
+  describe 'inventory management' do
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+    vendor3.stock(item1, 65)
+    vendor3.stock(item3, 10)
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+
+    it 'total_inventory counts quantity of items across all vendors' do
+      expect(market.total_inventory).to eq({
+        item1 => {quantity: 100, vendors: [vendor1, vendor3]},
+        item2 => {quantity: 7, vendors: [vendor1]},
+        item3 => {quantity: 35, vendors: [vendor2, vendor3]},
+        item4 => {quantity: 50, vendors: [vendor2]}
+        })
     end
   end
 end
