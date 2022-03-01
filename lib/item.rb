ed19d6081189ca66b88a859d9cec08f9ed1a2ddf
James Harkins <jh.harkins@protonmail.com>
2022-03-01 10:07:40 -0700

    feat: item class passing all tests

---
+++ lib/item.rb
@@ -0,0 +1,13 @@
+require 'pry'
+
+class Item
+
+  attr_reader :name,
+              :price
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @price = attributes[:price]
+  end
+
+end
