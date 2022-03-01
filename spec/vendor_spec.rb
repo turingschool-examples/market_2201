566157c747d6944b9d498975e5cc379669c36846
Johnny Bowman <johntbowman.production@gmail.com>
2022-03-01 10:50:45 -0700

    Test: Vendor calculates potential revenue

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -34,4 +34,12 @@ RSpec.describe Vendor do
     @vendor.stock(@item2, 12)
     expect(@vendor.inventory).to eq({@item1 => 55, @item2 => 12})
   end
+
+  it "calculates potential revenue" do
+    @vendor.stock(@item1, 30)
+    @vendor.stock(@item1, 25)
+    @vendor.stock(@item2, 12)
+
+    expect(@vendor.potential_revenue).to eq(47.75)
+  end
 end
