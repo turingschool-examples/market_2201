7ee1f273c49f30e86320ceb8e444fe182988bb6b
Will Medders <wmedders@gmail.com>
2022-03-01 12:57:37 -0500

    Spec add vendors

--- lib/market.rb
+++ lib/market.rb
@@ -1,6 +1,11 @@
 class Market
-  attr_reader :name
+  attr_reader :name, :vendors
   def initialize(name)
     @name = name
+    @vendors = []
+  end
+
+  def add_vendor(vendor)
+    @vendors << vendor
   end
 end
