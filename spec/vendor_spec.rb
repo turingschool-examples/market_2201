cab257e27596def6543db80d65c2433c75bb5e74
Zachary Imbriaco <zachary@imbriaco.com>
2022-03-01 12:32:02 -0500

    Complete iteration 1.

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -26,5 +26,10 @@ RSpec.describe Vendor do
       vendor.stock(item1, 25)
       expect(vendor.check_stock(item1)).to eq(55)
     end
+
+    it 'handles multiple stock items' do
+      vendor.stock(item2, 12)
+      expect(vendor.inventory).to eq({item1 => 55, item2 => 12})
+    end
   end
 end
