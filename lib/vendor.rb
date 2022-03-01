9fcaaddcea80b2effad8facdaa79855dd3348301
phdd0 <paul.haddick@outlook.com>
2022-03-01 12:03:47 -0700

    final notes: see comments on last fx worked: check_stock(): Vendor

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -10,9 +10,13 @@ class Vendor
     my_items_list_is = {}
   end
 
-#def check_stock(arg/item instance)
-  # default is zero
-#end
+def check_stock()
+
+ # so it's 1201...i need to pass an item instance<>
+ # from the particular vendor inventory hash list
+ # into a 0 defaulted variable, i think...
+
+end
 
 #def stock(arg/item instance, qty)
   # let's say we input 30 for qty
