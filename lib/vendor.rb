90ec932ce16beafa6044e13e52fe644a9935b3b2
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-01 10:18:57 -0700

    Test: Write tests for Vendor inventory management

---
+++ lib/vendor.rb
@@ -0,0 +1,9 @@
+require_relative 'item'
+
+class Vendor
+  attr_reader :inventory, :name
+  def initialize(name)
+    @name = name
+    @inventory = {}
+  end
+end
--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -1,4 +1,5 @@
 require 'rspec'
+require './lib/vendor'
 
 describe Vendor do
   describe 'initialization' do
@@ -10,6 +11,27 @@ describe Vendor do
 
     it 'starts with no items' do
       expect(vendor.inventory).to eq({})
+    end
+  end
+
+  describe 'stocking inventory' do
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
+    it 'check_stock returns 0 if there is no items' do
+      expect vendor.check_stock(item1).to eq(0)
+    end
+
+    it 'can check for a certain Item' do
+      vendor.stock(item1, 30)
+      expect(vendor.check_stock(item1)).to eq(30)
+
+      vendor.stock(item1, 25)
+      expect(vendor).check_stock(item1).to eq(55)
+
+      vendor.stock(item2, 12)
+      expect(vendor.check_stock(item2)).to eq(12)
     end
   end
 end
