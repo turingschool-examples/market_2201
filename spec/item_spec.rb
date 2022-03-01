9dcb23f4c831501c527ac8e1df37716d400999b8
Johnny Bowman <johntbowman.production@gmail.com>
2022-03-01 10:10:14 -0700

    Test: Item has readable attributes name and price

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -5,7 +5,13 @@ RSpec.describe Item do
   before (:each) do
     @item1 = Item.new({name: 'Peach', price: "$0.75"})
   end
+
   it "exists" do
     expect(@item1).to be_an_instance_of(Item)
+  end
+
+  it "has reaadable attributes 'name' and 'price'" do
+    expect(@item1.name).to eq("Tomato")
+    expect(@item1.price).to eq(0.5)
   end
 end
