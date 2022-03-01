2f9f67f6370221a97e88eaf780b3079888772ac1
Kim G <xiaole.guo@yahoo.com>
2022-03-01 10:08:06 -0700

    Test: Item exists and has attributes

---
+++ lib/vendor.rb
@@ -0,0 +1,5 @@
+class Vendor
+
+
+
+end
---
+++ spec/vender_spec.rb
@@ -0,0 +1,24 @@
+require '../lib/vendor'
+
+
+RSpec.describe Vendor do
+
+  before(:each) do
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+
+  it 'items ' do
+
+
+  end
+
+
+
+
+
+
+
+
+
+end
