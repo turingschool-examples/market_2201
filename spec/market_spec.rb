e0a2bf05eb41ad179607fd99d3b232f1b5429ac4
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-01 10:56:27 -0700

    Test: write tests for add_vendor method

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -4,13 +4,34 @@ require './lib/market'
 describe Market do
   describe 'initialization' do
     market = Market.new("South Pearl Street Farmers Market")
-
     it 'has a name' do
       expect(market.name).to eq("South Pearl Street Farmers Market")
     end
 
     it 'starts with no vendors' do
       expect(market.vendors).to eq([])
+    end
+  end
+
+  describe 'add_vendor' do
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+    vendor3.stock(item1, 65)
+    it 'adds a given vendor to the vendors array' do
+      market.add_vendor(vendor1)
+      market.add_vendor(vendor2)
+      market.add_vendor(vendor3)
+      expect(market.vendors).to eq([vendor1, vendor2, vendor3])
     end
   end
 end
