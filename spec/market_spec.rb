1cc553bc9ca41070b9a1cf72908d7d5558c56a8b
Chris Romero <chrisleeromero1@gmail.com>
2022-03-01 11:48:52 -0700

    Test: add test for total inventory method

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -97,16 +97,32 @@ RSpec.describe Market do
        vendor2.stock(item3, 25)
        vendor3 = Vendor.new("Palisade Peach Shack")
        vendor3.stock(item1, 65)
-
        @market.add_vendor(vendor1)
        @market.add_vendor(vendor2)
        @market.add_vendor(vendor3)
        expect(vendor1.potential_revenue).to eq(29.75)
        expect(vendor2.potential_revenue).to eq(345.00)
        expect(vendor3.potential_revenue).to eq(48.75)
-
     end
 
+    it "can display items sold" do
+      item1 = Item.new({name: "Peach", price: "$0.75"})
+      item2 = Item.new({name: "Tomato", price: "$0.50"})
+      item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+      vendor1 = Vendor.new("Rocky Mountain Fresh")
+      vendor1.stock(item1, 35)
+      vendor1.stock(item2, 7)
+      vendor2 = Vendor.new("Ba-Nom-a-Nom")
+      vendor2.stock(item4, 50)
+      vendor2.stock(item3, 25)
+      vendor3 = Vendor.new("Palisade Peach Shack")
+      vendor3.stock(item1, 65)
+      vendor3.stock(item3, 10)
+      market.add_vendor(vendor1)
+      market.add_vendor(vendor2)
+      market.add_vendor(vendor3)
+      expect(@market.total_inventory).to eq()
+    end
 
   end
 
