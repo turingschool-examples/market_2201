5455f02642aa79398d9479d85d9cd9a915941734
Susan Boyd <sueboyd922@gmail.com>
2022-03-01 12:33:28 -0500

    create Market class

---
+++ lib/market.rb
@@ -0,0 +1,19 @@
+require './lib/vendor'
+
+class Market
+  attr_reader :name, :vendors
+
+  def initialize(name)
+    @name = name
+    @vendors = []
+  end
+
+  def add_vendor(vendor)
+    @vendors << vendor
+  end
+
+  def vendor_names
+    @vendors.map {|vendor| vendor.name}
+  end
+
+end
